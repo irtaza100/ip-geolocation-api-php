6a7dd5464326e080bee9c59b732517b972362ef8
irtaza100 <34660777+irtaza100@users.noreply.github.com>
2018-08-16 12:57:45 +0500

    Update ip-geolocation-api-php.php
    add support for timezone

--- ip-geolocation-api-php.php
+++ ip-geolocation-api-php.php
@@ -1,26 +1,69 @@
 <?php
-$clientIp = "YOUR_CLIENT_IP";
-$apikey = "YOUR_API_KEY";
+$clientIp = "CLIENT_IP";
+$apiKey = "YOUR_API_KEY";
+$fields = "FIELDS";
 
-$response = get_location($apikey, $clientIp);
+echo "<p>"; print_r("Geolocation response"); echo "</p>";
+
+$response = geo_location($apiKey, $clientIp, $fields);
+
+$latitude = "LATITUDE";
+$longitude = "LONGITUDE";
+$timezone = "TIMEZONE";
+
+echo "<p>"; print_r("TimeZone response"); echo "</p>";
+$response = geo_timezone($apiKey , $clientIp, $latitude, $longitude, $timezone);
+
 $resArr = array();
 $resArr = json_decode($response);
 echo "<pre>"; print_r($resArr); echo "</pre>";
-function get_location($apiKey, $ip = null) {
- $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip;
- $cURL = curl_init();
- curl_setopt($cURL, CURLOPT_URL, $url);
- curl_setopt($cURL, CURLOPT_HTTPGET, true);
+function geo_location($apiKey, $ip = null, $fields = null) {
+    if ($fields) {
+        $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip."&fields=".$fields;
+    } else {
+        $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip;
+    }
+    $cURL = curl_init();
+    curl_setopt($cURL, CURLOPT_URL, $url);
+    curl_setopt($cURL, CURLOPT_HTTPGET, true);
 
 curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
- 'Content-Type: application/json',
- 'Accept: application/json'
+    'Content-Type: application/json',
+    'Accept: application/json'
 ));
 
- $content = curl_exec($cURL);
+    $content  = curl_exec($cURL);
 
- curl_close($cURL);
+    curl_close($cURL);
 
- return $content;
+    return $content;
 }
+
+
+function geo_timezone($apiKey, $ip = null, $lat = null, $long = null, $timezone = null) {
+    if ($timezone) {
+        $url = "https://api.ipgeolocation.io/timezone?apiKey=".$apiKey."&ip=".$ip."&timezone=".$timezone;
+    } else if ($lat && $long){
+        $url = "https://api.ipgeolocation.io/timezone?apiKey=".$apiKey."&ip=".$ip."&lat=".$lat."&long=".$long;;
+    } else {
+        $url = "https://api.ipgeolocation.io/timezone?apiKey=".$apiKey."&ip=".$ip;
+    }
+
+    $cURL = curl_init();
+    curl_setopt($cURL, CURLOPT_URL, $url);
+    curl_setopt($cURL, CURLOPT_HTTPGET, true);
+
+    curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Accept: application/json'
+    ));
+
+    $content  = curl_exec($cURL);
+
+    curl_close($cURL);
+
+    return $content;
+}
+
+
 ?>
